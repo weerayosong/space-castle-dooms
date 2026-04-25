ec8f096abaec477239fc7b8244f8dd151bcf2f9a
weerayosong <weerayosong@outlook.com>
2026-04-26 02:09:41 +0700

    chore: setup react project with tailwind v4 and basic UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+function App() {
+    return (
+        <div className="flex flex-col w-full max-w-md mx-auto h-full border border-gray-200 bg-white sm:rounded-sm shadow-lg relative overflow-hidden">
+            <h1 className="text-3xl font-black text-center text-red-600 p-4 font-inter">
+                SPACE CASTLE DOOMS
+            </h1>
+            <p className="text-center font-sans">-- Setup React Project --</p>
+        </div>
+    );
+}
+
+export default App;
